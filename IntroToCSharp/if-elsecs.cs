cc22ebc1da820c54a4689e9e23c1582d897744b0
Jregal2023 <157036653+Jregal2023@users.noreply.github.com>
2025-03-19 11:02:09 -0400

    Update if-elsecs.cs
    edited with correct piece of code

--- IntroToCSharp/if-elsecs.cs
+++ IntroToCSharp/if-elsecs.cs
@@ -1,53 +1,21 @@
-﻿/*//Conditionals ie if-else
-//just like in python you can have a single if statement with no else ie single alternative
+﻿﻿/*//inputs are used by calling the Console.ReadLine() method
+//methods in this case will be different from python because we will use the dot notation ( . ) to call our methods/function
+//so instead of print() we will call Console.ReadLine()
+//also Console.ReadLine() does not have a prompting feature
 
-//relational operators to check values equal, greater than, etc...
-//>,<,<=,>=,==,!=
-//remember when comparing two values we use == instead of =... = assigns values == compares value
+Console.WriteLine("Please enter your name: ");
+//create a variable with the type we are looking for and store it
+String name = Console.ReadLine();
+//to concatonate or combine strings, the simplist method is to use the + symbol
+Console.WriteLine("Hello " + name);
 
-
-//let's create a program to see if someone is eligible to vote
-//just checking age
-
-int age = 19;
-//if statements use the following syntax: if(comparison){}
-//if statements get skipped if they are not true
-if (age >= 18)
-{
-    Console.WriteLine("You are old enough to vote");
-}
-//else - dual alternative - two choices
-//alse known as if-else
-else
-{
-    Console.WriteLine("You are not old enough to vote");
-}
-
-//compund operators - check if multiple conditions are true/false
-//and, or, nots. THey look different than python
-//and = & - both comparisons must be true for the if statement to run
-// or = || - only one of the comparisons must be true for the if statment to run
-// not = ! - take the resulting boolean value and flips it (true becomes false and false becomes true)
-
-//check to see if a person is a US citizen and above 18 years old to vote
-int votingAge = 15;
-String citizen = "y";
-if (votingAge >= 18 & citizen == "y")
-{
-    Console.WriteLine("You are old enough and you are a citizen you can vote");
-}
-//maybe we want to tell the use the reason why they cannot vote. We can use multi-alternative to do so.
-//aka else if statements
-else if (votingAge >= 18 & citizen == "n")
-{
-    Console.WriteLine("You are old enough to vote but not a citizen, You cannot vote");
-}
-else if (votingAge <= 17 & citizen == "y")
-{
-    Console.WriteLine("You are not old enough to vote, but you are a citizen, You cannot vote");
-
-}
-else
-{
-    Console.WriteLine(" You are niether old enough, and you are not a citizen, You cannot vote");
-}*/
+//parsing - converting from a string or unknown value to a int or float or arithmetic value
+//adding two numbers together the user gives us
+Console.WriteLine("Please give me a number to add: ");
+//you can parse by saying datatype.Parse()
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Please give me a second number: ");
+int number2 = int.Parse(Console.ReadLine());
+int answer = number1 + number2;
+Console.WriteLine("Your answer is " + answer);
+*/
